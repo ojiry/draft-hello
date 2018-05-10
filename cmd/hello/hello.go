f4c87fafb4887b8dd447de94f185be820a97c181
Ryoji Yoshioka <ryoji.yoshioka@quipper.com>
2018-05-10 21:27:34 +0900

    Add kubernetes version

--- cmd/hello/hello.go
+++ cmd/hello/hello.go
@@ -5,11 +5,14 @@ import (
 
 	draftVersion "github.com/Azure/draft/pkg/version"
 	helmVersion "k8s.io/helm/pkg/version"
+	kubeVersion "k8s.io/kubernetes/pkg/version"
 )
 
 func main() {
 	v := draftVersion.New()
+	kv := kubeVersion.Get()
 	fmt.Println("Draft Hello!")
 	fmt.Printf("draft: %v\n", v.SemVer)
+	fmt.Printf("kubernetes: %v\n", kv.String())
 	fmt.Printf("helm: %v\n", helmVersion.GetVersion())
 }
